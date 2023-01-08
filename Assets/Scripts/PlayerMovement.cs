6cd2f39538923245cf9e21af6a66f642d5106ecf
leilus <prettytigerr@gmail.com>
2023-01-08 13:37:33 +0100

    PlayerMovement less sloppy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,47 +4,71 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed;
-
     public Transform orientation;
+    public float moveSpeed;
+    public float normalSpeed;
+    public float slowSpeed;
+    public KeyCode slowKey = KeyCode.LeftShift; 
 
     float horizontalInput;
     float verticalInput;
-
+ 
     Vector3 moveDirection;
+    
+    public Rigidbody rb;
 
-    Rigidbody rb;
 
     private void Start()
     {
+
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+       
     }
-
     private void Update()
     {
+        
+
         myInput();
+        whichMovement();
+
     }
+
+    private void whichMovement()
+    {
+        if (Input.GetKey(slowKey))
+        {
+            moveSpeed = slowSpeed;
+            rb.AddForce(moveDirection / slowSpeed, ForceMode.Force);
+           
+        }
+        else
+        {
+            moveSpeed = 20f;
+
+        }
+    }
+
+
+
 
     private void FixedUpdate()
     {
-        MovePlayer();
+        Move();
     }
+
 
 
     private void myInput()
     {
-
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
     }
 
-    private void MovePlayer()
+    public void Move()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        rb.AddForce(moveDirection.normalized * moveSpeed * 2f, ForceMode.Force);
+        rb.AddForce (moveDirection * moveSpeed , ForceMode.Force);
     }
-
 
 }
